4e9c67594f64ef67d75e25ea72fc3d3e7961892b
MagmusTop <jujupopobubu@gmail.com>
2025-12-09 21:14:39 +0100

    pages auth

--- lib/navigation.dart
+++ lib/navigation.dart
@@ -1,5 +1,9 @@
 import 'dart:developer';
 import 'package:code_initial/presentation/pages/register/register_page.dart';
+import 'package:code_initial/presentation/pages/auth/code_verification/code_verification_page.dart';
+import 'package:code_initial/presentation/pages/auth/code_verification/code_verification_controller.dart';
+import 'package:code_initial/presentation/pages/auth/mot_de_passe_oublie/mot_de_passe_oublie_controller.dart';
+import 'package:code_initial/presentation/pages/auth/mot_de_passe_oublie/mot_de_passe_oublie_page.dart';
 import 'package:get/get.dart';
 
 class Nav {
@@ -8,7 +12,16 @@ class Nav {
       name: Routes.REGISTER,
       page: () => RegisterPage(),
     ),
-
+    GetPage(
+      name: Routes.OTPCODE,
+      page: ()=>CodeVerificationPage(),
+      binding:CodeVerificationBinding(),
+    ),
+    GetPage(
+      name: Routes.MDPFORGET,
+      page: ()=>MotDePasseOubliePage(),
+      binding:MotDePasseOublieBinding(),
+    ),
   ];
 }
 
@@ -16,11 +29,13 @@ class Nav {
 class Routes {
   static Future<String> get initialRoute async {
 
-      return REGISTER;
+      return MDPFORGET;
   }
 
   static const String MAIN = "/";
 
   static const REGISTER = '/register';
+  static const OTPCODE = "/auth/code_verification";
+  static const MDPFORGET ="/auth/mot_de_passe_oublie";
 
 }
